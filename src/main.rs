59c196d3e9f615c72e585b0b953e09357b4d7d7f
watermelon <neefoxmo@gmail.com>
2018-07-25 15:35:17 +0800

    feat: add and guess the little game code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, watermelon!");
+
+    loop {
+        println!("\n---------------------Guess the number!\n");
+
+        let secret_number = rand::thread_rng().gen_range(1, 101);
+        loop {
+            println!("Please input your guess:");
+
+            let mut guess = String::new();
+
+            io::stdin().read_line(&mut guess)
+                .expect("failed to read line");
+
+            let guess: u32 = guess.trim().parse()
+                .expect("Please type a number!");
+
+            match guess.cmp(&secret_number) {
+                Ordering::Less    => println!("The number you guessed is too small!\n"),
+                Ordering::Greater => println!("The number you guessed is too big!\n"),
+                Ordering::Equal   => {
+                    println!("You win!\n");
+                    break;
+                }
+            }
+        }
+        println!("---------------------Yes,the secret number is: {}\n", secret_number);
+    }
 }
